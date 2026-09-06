998d9ad5835a8e6dfd593b8af95eff8cb53abf25
Thoki-Buthelezi <thokomanb@gmail.com>
2026-09-06 18:25:00 +0200

    added new attention computation

--- CAPSTONE/models/model.py
+++ CAPSTONE/models/model.py
@@ -52,12 +52,9 @@ class CausalSelfAttention(nn.Module):
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
-        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        att = att.masked_fill(self.mask[:, :, :T, :T] == 0, float("-inf"))
-        att = F.softmax(att, dim=-1)
-        att = self.attn_dropout(att)
-        y = att @ v
-        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        att = F.scaled_dot_product_attention(q, k, v, dropout_p=self.attn_dropout.p if self.training else 0.0, 
+                                             is_causal=True)
+        y = att.transpose(1, 2).contiguous().view(B, T, C)
         return self.resid_dropout(self.proj(y))
 
 
